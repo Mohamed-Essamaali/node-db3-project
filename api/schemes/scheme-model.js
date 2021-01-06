885a511f2fbb9a770d259339e448ba4bae4e8464
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2021-01-05 22:32:48 -0500

    mvp completed

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,48 @@
 // scheme-model
+const db = require('../../data/db-config')
+
+
+const find =()=>{
+    return db('schemes')
+
+}
+
+// find schemes
+const findById = id=>{
+return db('schemes').where('id',id).first()
+}
+
+//find steps based on schemeID
+const findSteps = schemesId =>{
+
+    return db('schemes as s')
+        .innerJoin('steps','s.id','steps.scheme_id')
+        .where('s.id',schemesId)
+        .select('steps.id','steps.step_number','s.scheme_name','steps.instructions')
+}
+
+//add scheme
+const add = schemData =>{
+    return db('schemes').insert(schemData)
+}
+
+//add steps
+const addStep = (stepData,id)=>{
+
+    return db('steps')
+    .innerJoin('schemes as s','s.id','steps.scheme_id')
+    .insert(stepData)
+    .where('steps.scheme_id',id)
+}
+
+//update scheme
+const update=(updated,id)=>{
+    return db('schemes').update(updated).where('id',id)
+}
+
+//remove scheme
+const remove = id=>{
+    return db('schemes').where('id',id).del()
+}
+
+module.exports = {find,findById,findSteps,add,addStep,update,remove}
--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,6 @@
 // db-config
+const knex = require('knex')
+
+const knexfile = require('../knexfile')
+
+module.exports = knex(knexfile.development)
